6f2205211774b9a743028cfff9ce3b8bf160fcde
Putta, Vishnu <vishnu.reddy@anthem.com>
2022-07-04 19:56:53 -0500

    Adding Fibonacci solution

---
+++ Fibonacci_Number.py
@@ -0,0 +1,12 @@
+# The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+class Solution:
+    def fib(self, n: int) -> int:
+        if n < 0:
+            print("Pass the positive integer values!")
+        elif n == 0:
+            return 0
+        elif n == 1 or n == 2:
+            return 1
+        else:
+            return self.fib(n-1) + self.fib(n-2)
