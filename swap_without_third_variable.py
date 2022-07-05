dd69fa3d65c121899239f99915d256e519b01068
Putta, Vishnu <vishnu.reddy@anthem.com>
2022-07-05 18:35:16 -0500

    adding logic for swap

---
+++ swap_without_third_variable.py
@@ -0,0 +1,13 @@
+
+def swap_numbers(i : int, j: int):
+    print("swap numbers")
+    i = i + j
+    print('i', i)
+    j = i - j
+    print('j', j)
+    i = i - j
+    print('i', i)
+
+
+
+swap_numbers(10,20)
